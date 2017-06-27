e05580403334499e01798a446ccb3d58857c3273
Sunil  Chaudhary <sunilchaudhary2304@gmail.com>
2017-06-26 22:09:05 -0500

    Add files via upload

---
+++ TwitterBar/src/wap/project/GetTweet.java
@@ -0,0 +1,21 @@
+/*package wap.project;
+
+import twitter4j.TwitterException;
+
+public class GetTweet {
+
+	public GetTweet() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String [] args) throws TwitterException{
+		GetTweets tweets = new GetTweets();
+		tweets.acceptTweet("Hillary").forEach((temp)->{
+			System.out.println(temp);
+		});
+		GetTweets.acceptTweet("Hillary").forEach((temp)->{
+			System.out.println(temp);
+		});		
+	}
+}
+*/
